ac0a9aff906bd6ccc89e401cb4d8cea70617ab86
mrdunghr <mrdunghr@gmail.com>
2024-03-23 16:54:02 +0700

    auto emo lol winfrom

---
+++ Auto_Emo_LOL/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Auto_Emo_LOL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "duong dan";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Auto Thông thạo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(15, 63);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Auto cười";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 210);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Emotion League Of Legend";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Auto_Emo_LOL/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto_Emo_LOL
+{
+    public partial class Form1 : Form
+    {
+        private Object reg;
+        private string path;
+        private JObject jsonObject;
+        public Form1()
+        {
+            InitializeComponent();
+            reg = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\Riot Game league_of_legends.live", "InstallLocation", null);
+            path = reg + "/Config/PersistedSettings.json";
+            label1.Text = path;
+
+            string jsonContent = File.ReadAllText(path);
+            jsonObject = JObject.Parse(jsonContent);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                AutoChampMastery(jsonObject, path, true);
+            }
+            else
+            {
+                AutoChampMastery(jsonObject, path, false);
+            }
+        }
+
+        private void checkBox2_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                AutoEmoteLaugh(jsonObject, path, true);
+            }
+            else
+            {
+                AutoEmoteLaugh(jsonObject, path, false);
+            }
+        }
+
+        private static void AutoChampMastery(JObject jsonObject, string path, bool status)
+        {
+            // tìm đến đối tượng có tên "evtChampMasteryDisplay" - thông thạo tướng
+            JToken targetSetting = jsonObject.SelectToken("$.files[?(@.name == 'Input.ini')].sections[?(@.name == 'GameEvents')].settings[?(@.name == 'evtChampMasteryDisplay')]");
+            Console.WriteLine("OLD");
+            Console.WriteLine(targetSetting);
+
+            // kiểm tra xem có tìm thấy đối tượng không
+            if (targetSetting != null)
+            {
+                // thay đổi giá trị của trường "value"
+                string value = status ? "[q], [w], [e], [r], [a], [s], [d], [f]" : "[Ctrl][6]";
+                targetSetting["value"] = value;
+            }
+            Console.WriteLine("NEW");
+            Console.WriteLine(targetSetting);
+
+            // Lưu lại các thay đổi vào file JSON
+            File.WriteAllText(path, jsonObject.ToString());
+        }
+        private static void AutoEmoteLaugh(JObject jsonObject, string path, bool status)
+        {
+            // tìm đến đối tượng có tên "evtChampMasteryDisplay" - thông thạo tướng
+            JToken targetSetting = jsonObject.SelectToken("$.files[?(@.name == 'Input.ini')].sections[?(@.name == 'GameEvents')].settings[?(@.name == 'evtEmoteLaugh')]");
+            Console.WriteLine("OLD");
+            Console.WriteLine(targetSetting);
+
+            // kiểm tra xem có tìm thấy đối tượng không
+            if (targetSetting != null)
+            {
+                // thay đổi giá trị của trường "value"
+                string value = status ? "[s], [d], [f]" : "[Ctrl][4]";
+                targetSetting["value"] = value;
+            }
+            Console.WriteLine("NEW");
+            Console.WriteLine(targetSetting);
+
+            // Lưu lại các thay đổi vào file JSON
+            File.WriteAllText(path, jsonObject.ToString());
+        }
+
+        
+    }
+}
